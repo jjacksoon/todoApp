26877dfd5903d47db3a39415f0b71b387d274785
Jackson Nascimento <jsnascimento1@hotmail.com>
2022-06-01 14:47:22 -0300

    Criando a função para deletar uma única todo de cada vez

--- components/ListItems.js
+++ components/ListItems.js
@@ -14,14 +14,25 @@ import { Entypo } from '@expo/vector-icons'
 const ListItems = ({todos, setTodos}) => {
   
   const[swipeRow, setSwipeRow] = useState(null);
-  
+
+  const handleDeleteTodo = (rowMap, rowKey) => {
+    const newTodos = [...todos];
+    const todoIndex = todos.findIndex((todo) => todo.key=== rowKey);
+    newTodos.splice(todoIndex,1);
+    setTodos(newTodos);
+  }
   return(
     <SwipeListView 
       data={todos}
       renderItem={(data) => {
         const RowText = data.item.key === swipeRow ? SwipedTodoText : TodoText;
         return(
-          <ListView>
+          <ListView
+            underlayColor = {colors.primary}
+            onPress={() => {
+
+            }}
+          >
             <>
               <RowText>{data.item.title}</RowText>
               <TodoDate>{data.item.date}</TodoDate>
@@ -29,10 +40,13 @@ const ListItems = ({todos, setTodos}) => {
           </ListView>
         )
       }}
-      renderHiddenItem={() => {
+      renderHiddenItem={(data, rowMap) => {
         return(
           <ListViewHidden>
-            <HiddenButton>
+            <HiddenButton
+              onPress={() => {
+                handleDeleteTodo(rowMap, data.item.key)}}
+            >
               <Entypo name='trash' size={25} color={colors.secondary}/>
             </HiddenButton>
           </ListViewHidden>
