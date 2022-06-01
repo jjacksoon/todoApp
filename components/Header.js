04d7aba3bb0282e62ff7dbcc11714e022923fb22
Jackson Nascimento <jsnascimento1@hotmail.com>
2022-06-01 11:10:35 -0300

    Criação do component Header

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  HeaderView,
+  HeaderTitle,
+  HeaderButton,
+  colors
+} from './styles/appStyles';
+import { Entypo } from '@expo/vector-icons';
+
+const Header = () =>{
+  return(
+    <HeaderView>
+      <HeaderTitle>Todos</HeaderTitle>
+      <HeaderButton>
+        <Entypo name='trash' size={25} color={colors.tertiary}/>
+      </HeaderButton>
+    </HeaderView>
+  );
+}
+
+export default Header;
